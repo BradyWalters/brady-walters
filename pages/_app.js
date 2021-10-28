c3766df94acf6a0279cd592e3251464e6070f514
Brady Walters <brady.walters5@gmail.com>
2021-10-28 09:40:55 -0700

    fixed font issue

--- pages/_app.js
+++ pages/_app.js
@@ -4,9 +4,6 @@ import Layout from '../components/layout'
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <style>
-          @import url('https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap')
-      </style> 
       <Component {...pageProps} />
     </Layout>
   )
