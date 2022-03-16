bba5ad92e015b4479d4c2977cc66133b9af33f48
Brady Walters <brady.walters5@gmail.com>
2022-03-16 13:39:09 -0700

    working on getting a dark mode, only thing is MUI Cards aren't affected

--- components/top-links.js
+++ components/top-links.js
@@ -1,12 +1,14 @@
 import SidebarLinks from "./sidebar-links";
 import SocialLinks from "./social-links";
 import styles from './top-links.module.css'
+import ThemeToggle from "./theme-toggle";
 
 export default function TopLinks() {
     return (
         <div className={styles.links}>
             <SidebarLinks />
             <SocialLinks />
+            <ThemeToggle />
         </div>
     )
 }
